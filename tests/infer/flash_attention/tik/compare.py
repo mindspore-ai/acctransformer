af0744dcbf24f0c6dbface58fdf7eed4ff8a4f18
zhangshuren <zhangshuren@huawei.com>
2023-12-22 19:28:58 +0800

    增加README文档以及修改测试对比函数

--- tests/infer/flash_attention/tik/compare.py
+++ tests/infer/flash_attention/tik/compare.py
@@ -14,36 +14,46 @@
 
 import numpy as np
 
-golden = np.load("./golden.npy")
-output = np.load("./result/fa_tik/query_0.npy")
 
-print(golden.shape)
-print(output.shape)
+def data_compare(ground_truth, predict, rtol=0.001, rtol_pct_thd=0.001, atol=0.001, max_rtol=0.1):
+    total_count = np.prod(ground_truth.shape)
+    greater_than_diff_thd_count = np.sum(
+        np.abs(predict - ground_truth) > rtol * np.abs(ground_truth) + atol
+    )
+    greater_than_max_diff_thd_count = np.sum(
+        np.abs(predict - ground_truth) > max_rtol * np.abs(ground_truth) + atol
+    )
 
-golden = golden.flatten()
-output = output.flatten()
-count = golden.shape[0]
+    diff_gt_thd_proportion = greater_than_diff_thd_count / total_count
+    diff_gt_max_thd_proportion = greater_than_max_diff_thd_count / total_count
+    result = "Pass"
+    if diff_gt_thd_proportion > rtol_pct_thd or diff_gt_max_thd_proportion > 0:
+        result = "Failed"
+    return result, diff_gt_thd_proportion, diff_gt_max_thd_proportion
 
-threhold = 0.001
-max_diff = 0
-errcnt_0 = 0
-errcnt_1 = 0
 
-for i in range(count):
-    gap = abs(golden[i] - output[i])
-    if gap > threhold:
-        errcnt_0 = errcnt_0 + 1
-    if gap > abs(threhold * golden[i]):
-        errcnt_1 = errcnt_1 + 1
-    if gap > max_diff:
-        max_diff = gap
+def display(ground_truth, predict, result, proportion1, proportion2, thd1=0.001, thd2=0.1):
+    print(
+        f"Compare result: {result}, relative diff greater than {thd1} proportion: {proportion1}, "
+        f"relative diff greater than {thd2} proportion: {proportion2}"
+    )
+    diff_max = np.max(np.abs(predict - ground_truth))
+    diff = np.abs(predict - ground_truth)
+    index = np.unravel_index(np.argmax(diff, axis=None), diff.shape)
+    index = tuple(map(int, index))
+    print(f"max diff {diff_max}, ground_truth: {ground_truth[index]}, predict: {predict[index]}")
 
-if count > 0:
-    err_ratio_0 = errcnt_0 / count * 100
-    err_ratio_1 = errcnt_1 / count * 100
-    print("count: " + str(count))
-    print("error count 0: " + str(errcnt_0))
-    print("error ratio 0: " + str(err_ratio_0) + "%")
-    print("error count 1: " + str(errcnt_1))
-    print("error ratio 1: " + str(err_ratio_1) + "%")
-    print("max gap value: " + str(max_diff))
+
+def check():
+    golden = np.load("./golden.npy")
+    output = np.load("./result/fa_tik/query_0.npy")
+
+    print(golden.shape)
+    print(output.shape)
+
+    result, diff_gt_rtol_ratio, diff_gt_max_rtol_ratio = data_compare(golden, output, rtol=0.01, rtol_pct_thd=0.01)
+    display(golden, output, result, diff_gt_rtol_ratio, diff_gt_max_rtol_ratio)
+
+
+if __name__ == '__main__':
+    check()
