b6debbced7ef9435a19e601b75da7c08a9146aef
zhangshuren <zhangshuren@huawei.com>
2024-01-16 18:48:27 +0800

    FA1上传

--- infer/build_infer/set_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# -*- coding: utf-8 -*-
-
-current_script_dir=$(realpath $(dirname -- $0))
-export ASCEND_CUSTOM_OPP_PATH=${current_script_dir}/pkg/vendors/customize:$ASCEND_CUSTOM_OPP_PATH
